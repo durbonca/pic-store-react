a2de392fe91face627536bc9de952ea317fe33df
Manuel Duran <durbonca@gmail.com>
2021-03-29 12:18:19 -0300

    logic remove button on cartItem

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,13 @@
+import {useContext} from "react"
+import {Context} from "../context/context"
+
 export default function CartItem ({img}){
+
+    const {removePhotoFromCart} = useContext(Context) 
+
     return(
         <div className="cart-item">
-            <i className="ri-delete-bin-line"></i>
+            <i className="ri-delete-bin-line" onClick={() => removePhotoFromCart(img.id)}></i>
             <img src={img.url} width="130px" alt="img"/>
             <p>$5.99</p>
         </div>
